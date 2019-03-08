135ecaf07eacb1a959ea504c36c85a55d7597d81
Derek <45375006+derekgeorg@users.noreply.github.com>
2019-03-07 22:31:53 -0600

    psuedo coded my game.js file. going to start hacking away.

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+//Randomly select a "computerScore" between 19-120.
+//Display random "computerScore" under Computer Score card.
+//Randomly assign a value between 1-12 to each crystal: #blueCrystal, #greenCrystal, #redCrystal, #yellowCrystal
