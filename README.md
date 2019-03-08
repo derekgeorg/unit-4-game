faab7b14b52219b4cda3c978a288936b63148413
Derek <45375006+derekgeorg@users.noreply.github.com>
2019-03-07 23:00:34 -0600

    added some documentation to readme.md and created someglobal variables in game.js.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # unit-4-game
 Homework #4
+
+CrystalCollect is a game assigment using jquery.
+
+I've based the design around Bootstrap elements. The predifined classes may cause some issue but has yet to be determined.
+
+Premise of the game is to match your score with a randomly generated computer score. Number will be chosen upon the start of each game and will be between 19-120.
+
+you increase your score by clicking a crystal, each with a randomly generated value of 1-12 set each game.
+
+If by determining the value of the cyrstals you can match you score to that of the computer, you win!
+
+if you go over the computer's score, you lose :(
+
+Wins and losses will be tallied at the bottom of the page.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,33 @@
-//Randomly select a "computerScore" between 19-120.
-//Display random "computerScore" under Computer Score card.
-//Randomly assign a value between 1-12 to each crystal: #blueCrystal, #greenCrystal, #redCrystal, #yellowCrystal
+//Variables that set the initial state of our CrystalsCollector game.
+var computerScore
+var yourScore
+var wins
+var losses
+var blueCrystalValue
+var greenCrystalValue
+var redCrystalValue
+var yellowCrystalValue
+var min = 19
+var max = 120
+var valueMin = 1
+var valueMax = 12
+
+//The gameSetup method is called when the page first loads.
+object.onload = function() {
+    var compNumber = Math.floor(Math.random() * 120) + 19;
+    console.log(compNumber);
+}
+    //Here we generate a random number for computerScore
+
+//Each game
+    //Randomly select a "computerScore" between 19-120.
+    //Display random "computerScore" under Computer Score card.
+    //Randomly assign a value between 1-12 to each crystal: #blueCrystal, #greenCrystal, #redCrystal, #yellowCrystal
+
+//.onClick of each crystal, take value and add to "yourCounter" total
+
+//if "yourCounter" = "computerScore"; alert: you win! wins++
+//else "yourCounter" > "computerScore"; alert: you lose. losses++
+
+//reset after win++ or losses++
+//upon reset change "yourCounter" === 0 and generate a new computerScore.
